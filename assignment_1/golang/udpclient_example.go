9f55b4a0f8551ee5ace881d5cb2bd43c48a4c22f
Val Lyashov <val@plstr.com>
2014-03-25 15:16:43 +1100

    codeship prep

--- assignment_1/golang/udpclient_example.go
+++
@@ -1,2 +0,0 @@
-serverAddr, err := net.ResolveUDPAddr("udp", "192.168.1.1:69")
-con, err := net.DialUDP("udp", nil, serverAddr)
--- assignment_1/golang/udpserver_example.go
+++
@@ -1,5 +0,0 @@
-var buf [1024]byte
-addr, err := net.ResolveUDPAddr("udp", ":69")
-sock, err := net.ListenUDP("udp", addr)
-for {
-    rlen, remote, err := sock.ReadFromUDP(buf)
